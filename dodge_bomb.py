b50e2dd02783ff327898ed6167c36abf05644db9
c0a2413549 <c0a2413549@edu.teu.ac.jp>
2025-09-30 14:47:27 +0900

    練習4:こうかとんと爆弾の衝突判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -49,7 +49,9 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
-
+        if kk_rct.colliderect(bb_rct):
+            return 
+        
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key, mv in DELTA.items():
